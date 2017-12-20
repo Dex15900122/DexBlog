618712a3ea57974e5236d0c559475b566713bd89
admin <admin@admins-MacBook-Pro.local>
2017-12-20 14:57:31 +0800

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(title:"My Blog Post #{blog}",
+                body:"If you're used to using raw SQL to find database records, then you will generally find that there are better ways to carry out the same operations in Rails. Active Record insulates you from the need to use SQL in most cases. "
+                )
+end
+
+puts "10 blog post created"
+
+5.times do |skill|
+  Skill.create!(title: "rails #{skill}",
+                percent_utilized: 15
+                )
+end                  
+
+puts "5 skill created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title #{portfolio_item}",
+    subtitle: "My great service",
+    body: "qwewqeqweqwdstyht",
+    main_image: "http://via.placeholder.com/600x300",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
+puts "9 portfolio items created"
