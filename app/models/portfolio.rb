dffdb1b937cc7054252bb7b072acd53c88dee454
admin <admin@admins-MacBook-Pro.local>
2018-01-04 22:15:39 +0800

    image default

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,4 +8,12 @@ class Portfolio < ApplicationRecord
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on rails')}
 
 
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x300"
+    self.thumb_image ||= "http://via.placeholder.com/350x200"
+  end
+
+
 end
