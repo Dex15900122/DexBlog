2e11a144750382a24b36dac716150f9a3a3dc320
admin <admin@admins-MacBook-Pro.local>
2018-01-09 12:04:38 +0800

    added layouts for blogs and portfolios

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,13 @@
+module DefaultPageContent
+  extend ActiveSupport::Concern
+
+    included  do
+      before_action :set_title
+    end
+    def set_title
+    @page_title = "Dex Portfolio || My Portfolio website "
+    @seo_keywords = "Dex Kuo Portfolio"
+    end
+end
+
+ 
