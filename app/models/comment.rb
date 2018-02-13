403d2b3ccb00b6bf449502c2807091d633200c52
Dex15900122 <you@example.com>
2018-02-13 12:45:51 +0800

    Action Cable part4-1

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :blog
 
-
   validates :content,presence: true, length: {minium:5 ,maximum:100}
+  after_create_commit { CommentBroadcastJob.perform_later(self) }
 end
