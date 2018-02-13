30a51020d16e9bda2c5fe992de07265c5badb282
Dex15900122 <you@example.com>
2018-02-13 12:44:07 +0800

    Action Cable part4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :comments
+  
+
   devise_for :users, path:'',path_names: {sign_in:'login',sign_out:'logout',sign_up:'register'}
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
@@ -11,7 +12,8 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
    get 'tech-news', to: 'pages#tech_news'
   
-  
+  mount ActionCable.server => '/cable'
+
   resources :blogs do
     member do
       get :toggle_status
